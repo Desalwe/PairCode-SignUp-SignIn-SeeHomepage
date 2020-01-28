dabb15eada52054046978b6aec551e1a70fe26e6
Edward Watson <e@edwardwatson.com|>
2020-01-28 19:51:54 +0000

    dbcreate

---
+++ db/create.js
@@ -0,0 +1,19 @@
+const connection = require("./config");
+
+const Users = `
+  CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    passwordHash CHAR(64) NOT NULL,
+    PRIMARY KEY (id)
+  );
+`;
+
+connection.query(Users, err => {
+  if (err) {
+    console.log(err);
+    connection.end();
+  } else {
+    console.log("users created");
+  }
+});
