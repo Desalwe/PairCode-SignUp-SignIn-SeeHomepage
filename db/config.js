8345ae55f7d130d4447aac25e3685bcdf11fd2b0
Alex Lawry-White <designalexweb@gmail.com>
2020-01-28 19:54:38 +0000

    m

--- db/config.js
+++ db/config.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql');
-const keys = require('../keys');
+const mysql = require("mysql");
+const keys = require("../keys");
 
 const connection = mysql.createConnection({
-    host:  'localhost', // address of the server
-    user:  keys.MYSQL_USERNAME, // username
-    password:  keys.MYSQL_PASSWORD,
-    database:  'books',
+  host: "localhost",
+  user: keys.MYSQL_USERNAME,
+  password: keys.MYSQL_PASSWORD,
+  database: "SignUpSignInProject"
 });
 module.exports = connection;
