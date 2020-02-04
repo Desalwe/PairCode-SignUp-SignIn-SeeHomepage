e608dfa50f2ee18ece693b9a18597f2052c7c005
Alex Lawry-White <designalexweb@gmail.com>
2020-02-04 19:41:58 +0000

    finish today

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -6,6 +6,7 @@ const User = require("../models/User");
 // };
 
 const createUser = (req, res) => {
+  console.log(req.body);
   User.create(req.body, err => {
     if (err) res.status(500).json({ flash: err.message });
     else res.status(200).json({ flash: "User created!" });
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,6 @@ const { createUser, loginUser } = require("../controllers/auth-controller");
 const router = express.Router();
 
 router.post("/signup", createUser);
-router.post("/login", loginUser);
+// router.post("/login", loginUser);
 
 module.exports = router;
